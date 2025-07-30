94c42ef0d9a7baa0738bb9f3e1a5c9c4bce88914
Muzammal Ghafoor <muzammalghafoor04@gmail.com>
2025-07-29 22:27:49 -0700

    Spotify Clone Done

---
+++ src/components/AlbumItem.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+
+const AlbumItem = ({image ,name ,desc,id}) => {
+
+  
+  const navigate=useNavigate()
+  return (
+    <div onClick={()=>navigate(`./album/${id}`)} className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
+    <img className='rounded' src={image} alt="" />
+    <p className='font-blod mt-2 mb-1 '>{name}</p>
+    <p className='text-slate-200 text-sm'>{name}</p>
+    </div>
+  )
+}
+
+export default AlbumItem;
---
+++ src/components/SongItem.jsx
@@ -0,0 +1,16 @@
+
+import React, { use, useContext } from 'react'
+import { playerContext } from '../context/PlayerContext'
+
+const SongItem = ({image ,name ,desc,id}) => {
+    const{playWithId}=useContext(playerContext)
+  return (
+    <div onClick={()=>playWithId(id)} className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
+    <img className='rounded' src={image} alt="" />
+    <p className='font-blod mt-2 mb-1 '>{name}</p>
+    <p className='text-slate-200 text-sm'>{desc}</p>
+    </div>
+  )
+}
+
+export default SongItem
